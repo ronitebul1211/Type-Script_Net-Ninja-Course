a8072c96579afb276f901455cf122eb3e22ae163
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-13 13:00:35 +0200

    separate sandbox into sections

--- sandbox.ts
+++ sandbox.ts
@@ -1,10 +1,12 @@
+/** Type Script Compiler */
 // const inputs = document.querySelectorAll("input");
 // inputs.forEach((input) => {
 //    console.log(input);
 // });
 
-const circ = (diameter: number) => {
-   return diameter * Math.PI;
-};
-console.log(circ("hello"));
-console.log(circ(1));
+/** Types Basics */
+// const circ = (diameter: number) => {
+//    return diameter * Math.PI;
+// };
+// console.log(circ("hello"));
+// console.log(circ(1));
